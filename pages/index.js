a700b6cc2c36a9e9cd9b99b5b1e6843a8db37f60
Carlos Efe Pardo <48070328+carlosfpardo@users.noreply.github.com>
2022-10-11 13:57:13 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>lestercorp es aquí!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to lestercorp! We are still working on our site, but hope to be around soon" />
+        <Header title="Lestercorp está aquí para ayudarle con sus necesidades. Estaremos pronto con ustedes dando mayor información en esta página" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+          </p>
       </main>
 
       <Footer />
